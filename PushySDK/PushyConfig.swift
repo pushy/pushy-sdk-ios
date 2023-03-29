8921f83449090650b4c7d99e2aeb22c450043d02
Pushy <support@pushy.me>
2023-03-29 14:22:16 +0200

    1.0.42

--- PushySDK/PushyConfig.swift
+++ PushySDK/PushyConfig.swift
@@ -13,5 +13,5 @@ public class PushyConfig {
     static var apiBaseUrl = "https://api.pushy.me"
     
     // SDK version code
-    static var sdkVersionCode = 1041;
+    static var sdkVersionCode = 1042;
 }
